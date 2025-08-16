4f9e7ea89e3a55ef1bcf708c99a1ebf803ac41ae
VitorMalvestiti <malvestiti.vitor@gmail.com>
2025-08-16 10:07:34 -0300

    test(logParser): teste para reconhcer kills de armar e do mundo

--- src/tests/unit/application/services/log-parser.service.spec.ts
+++ src/tests/unit/application/services/log-parser.service.spec.ts
@@ -1,5 +1,6 @@
+import { KillEvent } from "../../../../domain/entities/kill-event.entity";
 import { LogParserService } from "../../../../application/services/log-parser.service";
-import { Match } from 'src/domain/entities/match.entity';
+import { Match } from '../../../../domain/entities/match.entity';
 
 describe('LogParserService - básico', () => {
     let svc: LogParserService;
@@ -13,7 +14,7 @@ describe('LogParserService - básico', () => {
         expect(svc.parse('   \n  ')).toEqual({ matches: [], events: [] });
     });
 
-    it('reconhcer inicio e fim da partida', () => {
+    it('reconhecer inicio e fim da partida', () => {
         const log = `
       23/04/2019 18:34:22 - New match 1 has started
       23/04/2019 18:39:22 - Match 1 has ended
@@ -24,5 +25,26 @@ describe('LogParserService - básico', () => {
         expect(out.matches[0].id).toBe('1');
         expect(out.matches[0].startedAt.toISOString()).toBe('2019-04-23T18:34:22.000Z');
         expect(out.matches[0].endedAt?.toISOString()).toBe('2019-04-23T18:39:22.000Z');
+    });
+
+    it('reconhecer kills de arma e de mundo', () => {
+        const log = `
+      23/04/2019 18:34:22 - New match 1 has started
+      23/04/2019 18:36:04 - Roman killed Nick using M16
+      23/04/2019 18:36:33 - <WORLD> killed Nick by DROWN
+      23/04/2019 18:39:22 - Match 1 has ended
+    `;
+        const out = svc.parse(log);
+        expect(out.events.length).toBe(2);
+
+        const e1 = out.events[0] as KillEvent;
+        expect(e1.killer).toBe('Roman');
+        expect(e1.victim).toBe('Nick');
+        expect(e1.cause).toEqual({ type: 'WEAPON', weapon: 'M16' });
+
+        const e2 = out.events[1] as KillEvent;
+        expect(e2.killer).toBe('<WORLD>');
+        expect(e2.victim).toBe('Nick');
+        expect(e2.cause).toEqual({ type: 'WORLD', reason: 'DROWN' });
     });
 });
