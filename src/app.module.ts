370e0d71f4b6764b8528a854918c484b31a352b6
VitorMalvestiti <malvestiti.vitor@gmail.com>
2025-08-17 05:01:36 -0300

    feat(uploadModule): import uploadModule no AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { validationSchema } from './config/validation';
 import { typeormConfig } from './infrastructure/database/typeorm.config';
+import { UploadModule } from './infrastructure/modules/upload/upload.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validationSchema }),
     TypeOrmModule.forRootAsync(typeormConfig),
+    UploadModule,
   ],
 })
 export class AppModule { }
