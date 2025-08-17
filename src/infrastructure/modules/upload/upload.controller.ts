6bbe43d7a3d2338069d833ccdf23053e4de3d96a
VitorMalvestiti <malvestiti.vitor@gmail.com>
2025-08-17 05:00:14 -0300

    feat(upload): Create module e controller

---
+++ src/infrastructure/modules/upload/upload.controller.ts
@@ -0,0 +1,31 @@
+import { BadRequestException, Body, Controller, Post, Req, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ParseLogUseCase } from '../../../application/use-cases/parse-log.use-case';
+
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly parseLog: ParseLogUseCase) {}
+
+  @Post()
+async uploadJsonOrText(@Req() req: Request, @Body() body: any) {
+    console.log('REQ METHOD:', req.method);
+    console.log('REQ URL:', req.url);
+    console.log('HEADERS:', req.headers);
+    console.log('BODY:', body);
+
+    const raw = typeof body === 'string' ? body : body?.content;
+    const log = (raw ?? '').trim();
+    if (!log) throw new BadRequestException('Empty log content');
+
+    return this.parseLog.execute({ log });
+  }
+
+  @Post('file')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file?: Express.Multer.File) {
+    const log = file ? file.buffer.toString('utf8').trim() : '';
+    if (!log) throw new BadRequestException('Empty log content');
+    return this.parseLog.execute({ log });
+  }
+}
---
+++ src/infrastructure/modules/upload/upload.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UploadController } from './upload.controller';
+import { ParseLogUseCase } from '../../../application/use-cases/parse-log.use-case';
+import { LogParserService } from '../../../application/services/log-parser.service';
+import { StatsCalculatorService } from '../../../application/services/stats-calculator.service';
+import { MatchOrmEntity } from '../../database/orm/match.orm-entity';
+import { PlayerOrmEntity } from '../../database/orm/player.orm-entity';
+import { KillOrmEntity } from '../../database/orm/kill.orm-entity';
+import { AwardOrmEntity } from '../../database/orm/award.orm-entity';
+
+@Module({
+  imports: [
+    MulterModule.register({}),
+    TypeOrmModule.forFeature([MatchOrmEntity, PlayerOrmEntity, KillOrmEntity, AwardOrmEntity]),
+  ],
+  controllers: [UploadController],
+  providers: [ParseLogUseCase, LogParserService, StatsCalculatorService],
+})
+export class UploadModule {}
